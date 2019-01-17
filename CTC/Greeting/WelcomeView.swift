57e50ac77a0ca9255a338b8e73b884558f4b54ce
Dave Becker <dmb43016@gmail.com>
2019-01-17 14:19:58 -0500

    Added welcome text

--- CTC/Greeting/WelcomeView.swift
+++ CTC/Greeting/WelcomeView.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class WelcomeView: UIView {
 
+    lazy var welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 10, y: 10, width: 500, height: 200))
+        label.font = UIFont(name: "Futura-Medium", size: 45)
+        label.text = """
+                     Welcome
+                     To
+                     Code That Cares...
+                     """
+        label.textColor = .white
+        label.numberOfLines = 3
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1.0)
+        self.setupView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        self.addSubview(welcomeLabel)
     }
     
 }
