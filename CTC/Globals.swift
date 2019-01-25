5441210f7ba6f3a1bb2bf29d874e66d019f21b25
Dave Becker <dmb43016@gmail.com>
2019-01-25 17:24:34 -0500

    Some fixes and stuff

---
+++ CTC/Globals.swift
@@ -0,0 +1,21 @@
+//
+//  Globals.swift
+//  CTC
+//
+//  Created by Dave Becker on 1/22/19.
+//  Copyright © 2019 Code That Cares. All rights reserved.
+//
+
+import UIKit
+
+class Globals: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
