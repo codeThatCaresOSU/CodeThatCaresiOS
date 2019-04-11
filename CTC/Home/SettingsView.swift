e06e44b0a51fe61b6cd3a55397eccf6d3ea3aa34
Dave Becker <dmb43016@gmail.com>
2019-04-11 18:16:26 -0400

    Added settings view file

---
+++ CTC/Home/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  CTC
+//
+//  Created by Dave Becker on 4/11/19.
+//  Copyright © 2019 Code That Cares. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+
+}
